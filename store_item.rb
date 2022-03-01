d386622d627fc9363da871fd27cfa7b93417639d
TLCurd <TLCurdChiro@gmail.com>
2022-03-01 12:38:16 -0700

    first commit

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+# Tile Store
+
+tile1 = {:color => "brown", :size => 12, :material => "marble"}
+tile2 = {:color => "red", :size => 6, :material => "ceramic"}
+tile3 = {:color => "yellow", :size => 12, :material => "porcelain"}
+tile4 = {color: "brown"}
+
+p tile4[:color]
