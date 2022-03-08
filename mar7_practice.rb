ac8ed5908dd756950df41067956b14eaafd0052f
TLCurd <TLCurdChiro@gmail.com>
2022-03-07 19:06:33 -0700

    adds first loop for grouping names in pairs

--- mar7_practice.rb
+++ mar7_practice.rb
@@ -78,27 +78,50 @@
 
 
 
-users = []
+# users = []
 
-5.times do 
-  person = {}
-  puts "Please enter the first name."
-  person[:first_name] = gets.chomp
-  puts "Please enter the last name."
-  person[:last_name] = gets.chomp
-  puts "Please enter the email address."
-  person[:email] = gets.chomp
-  person[:account] = 10.times.map{rand(0..9).to_s}.join
-  users << person
-  end
-  users.each do |user|
-    puts "First name: #{user[:first_name]}"
-    puts "Last name: #{user[:last_name]}"
-    puts "Email: #{user[:email]}"
-    puts "ACCT #: #{user[:account]}"
-    puts "          "
-end
+# 5.times do 
+#   person = {}
+#   puts "Please enter the first name."
+#   person[:first_name] = gets.chomp
+#   puts "Please enter the last name."
+#   person[:last_name] = gets.chomp
+#   puts "Please enter the email address."
+#   person[:email] = gets.chomp
+#   person[:account] = 10.times.map{rand(0..9).to_s}.join
+#   users << person
+#   end
+#   users.each do |user|
+#     puts "First name: #{user[:first_name]}"
+#     puts "Last name: #{user[:last_name]}"
+#     puts "Email: #{user[:email]}"
+#     puts "ACCT #: #{user[:account]}"
+#     puts "          "
+# end
 
 # p users
 
 
+# Final Review
+# Create a program that puts your class into groups! Have the program request the user to enter each student’s name.  Assume the classroom has an even number of students, so there are only groups of two. For example, you can have the program output groups like so:
+# 	Group: Hermione Seamus
+# 			Group: Lucius Cho
+# 			Group: Sirius Luna
+# 			Group: Severus Draco
+
+# need to create an array within an array
+all = []
+# group = []
+puts "Please enter the number of students in your class."
+number = gets.chomp.to_i / 2
+number.times do
+  group = []
+  2.times do
+    puts "Please enter a student's name."
+    name = gets.chomp
+    group << name
+  end
+  all << group
+end
+
+p all
